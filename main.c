ebbe792c50a73bd299a91bf4eb4f19d0795f9486
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-04-12 23:54:39 +0530

    modified the sentence of result

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int main(void)
   struct Point organism_location = {36,27};
   struct Point closest_food;
   get_closest_food(food_locations, 5, organism_location, &closest_food);
-  printf("%d %d", closest_food.x, closest_food.y);
+  printf("Nearest food location is [%d %d]", closest_food.x, closest_food.y);
   return 0;
 }
