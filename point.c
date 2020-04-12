cf78ae5d7cfe488acc2c29c15c28ec53e4f15131
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-04-13 00:01:30 +0530

    modified get_closest_food

--- point.c
+++ point.c
@@ -1,13 +1,13 @@
 #include <math.h>
 #include "point.h"
 
-double find_distance(POINT *point1,POINT *point2){
+DISTANCE find_distance(POINT *point1,POINT *point2){
   return hypot(point2->x - point1->x, point2->y - point1->y);
 }
 
 void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location){
-  double shortest_distance = find_distance(&current_location, &food_points[0]);
-  double current_distance;
+  DISTANCE shortest_distance = find_distance(&current_location, &food_points[0]);
+  DISTANCE current_distance;
   *closest_food_location = food_points[0];
   
   for (int i = 0; i < points_length; i++)
